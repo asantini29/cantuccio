945f9df12e57a7d0d6bbe032e3321dce69e26ecb
Alessandro Santini <alesandro.santini@aei.mpg.de>
2026-05-14 16:02:20 +0200

    setting up CI for pypi

--- src/cantuccio/__init__.py
+++ src/cantuccio/__init__.py
@@ -7,4 +7,5 @@ from .core import cornerplot, overplot_lines, get_credible_interval
 
 __copyright__ = "2026, Alessandro Santini"
 __author__ = "Alessandro Santini"
+__email__ = "alessandro.santini@aei.mpg.de"
 __version__ = "0.1.0"
