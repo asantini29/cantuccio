21f2dcd0fb39eede6fb47ce47bd1dd51ae1862d2
Alessandro Santini <alesandro.santini@aei.mpg.de>
2026-05-18 10:02:36 +0200

    automatic versioning

--- src/cantuccio/__init__.py
+++ src/cantuccio/__init__.py
@@ -5,7 +5,13 @@ Cantuccio - Yet another corner plot package. The sweetest one around.
 from . import core, kde, visuals
 from .core import cornerplot, overplot_lines, get_credible_interval
 
+from importlib.metadata import version, PackageNotFoundError
+
 __copyright__ = "2026, Alessandro Santini"
 __author__ = "Alessandro Santini"
 __email__ = "alessandro.santini@aei.mpg.de"
-__version__ = "0.1.3"
+
+try:
+    __version__ = version("cantuccio")
+except PackageNotFoundError:
+    __version__ = "unknown"
